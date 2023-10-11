151b6d5c8d38480ab53988115e641d9271895b44
samBoyle <sboyle05@gmail.com>
2023-10-11 13:03:06 -0700

    Add Users table migration and seeds

---
+++ backend/db/seeders/20231011191431-demo-user.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'John',
+        lastName: 'Doe'
+      },
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: 'Jon',
+        lastName: 'Doey'
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName: 'Joey',
+        lastName: 'Dosh'
+      },
+      {
+        email: 'sboyle05@gmail.com',
+        username: 'admin-user',
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'Sam',
+        lastName: 'Boyle'
+      },
+
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2', 'admin-user'] }
+    }, {});
+  }
+};
